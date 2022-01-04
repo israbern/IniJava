5b6fa6192f96de61d498bd717d8d87813c4b16d6
David Bernabé <david.bern.pal@gmail.com>
2022-01-04 15:00:25 +0100

    Update CalculosMath.java
    Refactor blank spaces

--- src/CalculosConMath.java
+++ src/CalculosConMath.java
@@ -8,15 +8,10 @@ public class CalculosConMath {
         double base = 5;
         double exponente = 3;
 
-
-
         int resultado = (int)Math.pow(base, exponente);
         
-
-    System.out.println(" El resultado de " + base + " elevado a " + exponente + " es " + resultado);    
-            
-                
-        
+        System.out.println(" El resultado de " + base + " elevado a " + exponente + " es " + resultado);    
+           
     }
     
 }
